b93a8dbb9b3650d045362786fd418e844c435879
Squadros <eduardo.quadros@southsystem.com.br>
2020-04-13 15:23:38 -0300

    Ajust db

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://f11equadros:endycsmu@f11-w2cla.mongodb.net/f11?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://eduardosantana01:endycsmu@mongodb.eduardosantanass.kinghost.net:27017/eduardosantana01', { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
